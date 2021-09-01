f351596293cbd720132a8f44782bb9984190c9ba
Abdulqader Mohammed <abdulqader0010@gmail.com>
2021-09-01 22:34:56 +0800

    Replace classes

---
+++ src/main/java/CustomAuthenticatorFactory.java
@@ -0,0 +1,70 @@
+import java.util.List;
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+public class CustomAuthenticatorFactory implements AuthenticatorFactory {
+  @Override
+  public String getDisplayType() {
+    return null;
+  }
+
+  @Override
+  public String getReferenceCategory() {
+    return null;
+  }
+
+  @Override
+  public boolean isConfigurable() {
+    return false;
+  }
+
+  @Override
+  public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+    return new AuthenticationExecutionModel.Requirement[0];
+  }
+
+  @Override
+  public boolean isUserSetupAllowed() {
+    return false;
+  }
+
+  @Override
+  public String getHelpText() {
+    return null;
+  }
+
+  @Override
+  public List<ProviderConfigProperty> getConfigProperties() {
+    return null;
+  }
+
+  @Override
+  public Authenticator create(KeycloakSession keycloakSession) {
+    return null;
+  }
+
+  @Override
+  public void init(Config.Scope scope) {
+
+  }
+
+  @Override
+  public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  @Override
+  public String getId() {
+    return null;
+  }
+}
