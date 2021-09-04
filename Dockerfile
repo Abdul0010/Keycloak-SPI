9ecc35475503c1ce8b91470ed5ec35f52a3d3434
Abdulqader Mohammed <abdulqader0010@gmail.com>
2021-09-04 12:53:52 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM quay.io/keycloak/keycloak:15.0.2
 
-ARG AUTHENTICATOR_JAR=target/KeyclockSpi.jar
+
+ARG AUTHENTICATOR_JAR=target/Keycloak-SPI-1.0-SNAPSHOT.jar
 ENV JAVA_TOOL_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
 
 # copy the jars ...
-COPY ${AUTHENTICATOR_JAR} /opt/jboss/keycloak/standalone/deployments/
+#COPY ${AUTHENTICATOR_JAR} /opt/jboss/keycloak/standalone/deployments/
--- src/main/java/CustomAuthenticator.java
+++ src/main/java/CustomAuthenticator.java
@@ -1,18 +1,52 @@
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
-
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.services.messages.Messages;
+@Slf4j
 public class CustomAuthenticator implements Authenticator {
+
   @Override
-  public void authenticate(AuthenticationFlowContext authenticationFlowContext) {
+  public void authenticate(AuthenticationFlowContext context) {
 
   }
 
   @Override
-  public void action(AuthenticationFlowContext authenticationFlowContext) {
+  public void action(AuthenticationFlowContext context) {
+    MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
+    String username = formData.getFirst(Details.USERNAME);
+    String password = formData.getFirst(CredentialRepresentation.PASSWORD);
+    log.info("username "+username);
+    log.info("password "+password);
+    if (StringUtils.isBlank(username)) {
+      showErrorMessageOnLoginPage(context, Errors.USERNAME_MISSING, Messages.MISSING_USERNAME,
+          AuthenticationFlowError.INVALID_USER);
+    }
+    if (StringUtils.isBlank(password)) {
+      showErrorMessageOnLoginPage(context, Errors.PASSWORD_MISSING, Messages.MISSING_PASSWORD,
+          AuthenticationFlowError.INVALID_USER);
+    }
+    context.success();
 
+    //validate user by calling api
+  }
+  private void showErrorMessageOnLoginPage(AuthenticationFlowContext context,
+                                           String error, String errorMsg,
+                                           AuthenticationFlowError authenticationFlowError) {
+    context.getEvent().error(error);
+    Response challenge = context.form()
+        .setError(errorMsg)
+        .createLoginUsernamePassword();
+    context.failureChallenge(authenticationFlowError, challenge);
   }
 
   @Override
@@ -21,12 +55,12 @@ public class CustomAuthenticator implements Authenticator {
   }
 
   @Override
-  public boolean configuredFor(KeycloakSession keycloakSession, RealmModel realmModel, UserModel userModel) {
+  public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
     return false;
   }
 
   @Override
-  public void setRequiredActions(KeycloakSession keycloakSession, RealmModel realmModel, UserModel userModel) {
+  public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
 
   }
 
